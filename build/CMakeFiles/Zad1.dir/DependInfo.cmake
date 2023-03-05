fd398cdbadd81ad997c81ef3439271669c360848
Ilya Tsoy <inqlest@gmail.com>
2023-03-06 00:47:23 +0300

    add Cmake

---
+++ build/CMakeFiles/Zad1.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/vscode/HW-MF/Zad1/calculator.cpp" "CMakeFiles/Zad1.dir/calculator.cpp.obj" "gcc" "CMakeFiles/Zad1.dir/calculator.cpp.obj.d"
+  "C:/vscode/HW-MF/Zad1/main.cpp" "CMakeFiles/Zad1.dir/main.cpp.obj" "gcc" "CMakeFiles/Zad1.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/Zad1.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Zad1.dir/calculator.cpp.obj"
+  "CMakeFiles/Zad1.dir/calculator.cpp.obj.d"
+  "CMakeFiles/Zad1.dir/main.cpp.obj"
+  "CMakeFiles/Zad1.dir/main.cpp.obj.d"
+  "Zad1.exe"
+  "Zad1.exe.manifest"
+  "Zad1.pdb"
+  "libZad1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Zad1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
